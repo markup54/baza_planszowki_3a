c0cecb4bde560a0c78bfb9d079a92125a49bd9b8
markup54 <marzenakupka@tlen.pl>
2021-11-18 08:06:32 +0100

    dao zapisane

--- app/src/main/java/com/example/planszowki/PlanszowkaDao.java
+++ app/src/main/java/com/example/planszowki/PlanszowkaDao.java
@@ -1,4 +1,23 @@
 package com.example.planszowki;
 
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
 public interface PlanszowkaDao {
+    @Insert
+    void wstawPlanszowke(Planszowka gra);
+
+    @Insert
+    void wstawListePlanszowek(List<Planszowka> gry);
+
+    @Query("SELECT * FROM planszowki")
+    List<Planszowka> wszystkiePlanszowki();
+
+    @Query("SELECT * FROM planszowki WHERE wiek<=:w")
+    List<Planszowka> planszowkiWgWieku(int w);
+
+    @Query("SELECT * FROM planszowki WHERE minLiczbaOsob<=:l AND maxLiczbaOsob>=:l")
+    List<Planszowka> planszowkiWgLiczbyGraczy(int l);
 }
