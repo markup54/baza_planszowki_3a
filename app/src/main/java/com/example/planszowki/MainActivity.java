57e965a9a7624e0a52397abf56334e41c7087121
markup54 <marzenakupka@tlen.pl>
2021-11-18 09:52:28 +0100

    niedokonczona MainActivity

--- app/src/main/java/com/example/planszowki/MainActivity.java
+++ app/src/main/java/com/example/planszowki/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.planszowki;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PlanszowkiViewModel planszowkiViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        planszowkiViewModel = new ViewModelProvider(this,
+                new ViewModelProvider
+                        .AndroidViewModelFactory(getApplication()))
+                .get(PlanszowkiViewModel.class);
     }
 }
